05bdf63942db54a957e977cdaba99494fd878e89
Jim <shinymetallicpurplearmor@gmail.com>
2025-07-21 15:07:53 +0200

    fixed key name

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -6,6 +6,6 @@ output "connection_script" {
   value = templatefile("${var.host_os}-ssh-config.tpl", {
     hostname = aws_instance.dev_node.public_ip,
     user     = "ubuntu",
-  identityfile = "~/.ssh/mtckey" })
+  identityfile = "~/.ssh/tsakey" })
 }
 
